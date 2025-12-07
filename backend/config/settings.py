81d5305f1781d18f5b3073373d55246ab2ce658b
Hossein Taimoury <hosseintaimoury@gmail.com>
2025-12-07 14:06:01 +0330

    Initialize the project and create users app

---
+++ backend/config/settings.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+from datetime import timedelta
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-pb1ve5@$0=hp@9yt6mrhda%+4+*d0ygq&_%mw61zp$^t(_x&4u"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "users",
+]
+
+
+# OPTIONAL: Add configuration settings for spectacular
+# This is not required but can be helpful for branding/metadata
+SPECTACULAR_SETTINGS = {
+    "TITLE": "My Blog CBVs API",
+    "DESCRIPTION": "Detailed documentation for the Blog REST API",
+    "VERSION": "1.0.0",
+    # This setting tells Spectacular where to find the API schema generation logic
+    "SERVE_INCLUDE_SCHEMA": False,
+    "COMPONENT_SPLIT_REQUEST": True,
+}
+
+# Specifying my custom user model
+AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    # This is for Django project to be configured to use 'Simple JWT' auth library.
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    # The following line is for drf_spectacular library to work
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+SIMPLE_JWT = {
+    # ... other default settings ...
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),  # Changed from minutes=5
+    # The refresh token lifetime will remain at the default (1 day)
+}
+
+# Settings to store post images locally in development phase 👇
+
+# Absolute path to the directory where user-uploaded media files will be stored.
+# This creates a 'media/' folder at the root of your project.
+MEDIA_ROOT = BASE_DIR / "media"
+
+# The URL prefix for media files (e.g., accessed at http://127.0.0.1:8000/media/...)
+MEDIA_URL = "/media/"
+
+# Note: In production (AWS S3), you would change or override these settings.
+
+
+# Settings to store post images locally in development phase 👆
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/6.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STATIC_URL = "static/"
---
+++ backend/config/urls.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.urls import path, include
+
+# The following import is here for drf_spectacular library
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+# The following two imports are here for serving post images during development
+from django.conf import settings  # Import settings
+from django.conf.urls.static import static  # Import static function
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/users/", include("users.urls")),
+    # path("api/posts/", include("posts.urls")),
+    # The following two URL patterns are here for drf_spectacular library
+    # API Schema View (The raw JSON/YAML specification file)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI View (The interactive documentation interface)
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
+
+# 💥 SERVE MEDIA FILES ONLY IN DEVELOPMENT 💥
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
